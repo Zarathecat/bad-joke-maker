2fff2a947ecbd9aff643a927980a1753a3204081
Zara <thezarazia@gmail.com>
2017-03-18 02:04:51 +0000

    Explain the rules of the bad joke maker

---
+++ badjokemaker.py
@@ -0,0 +1,12 @@
+""" This will be a bad joke maker.
+"What is a word that means [word1.definition]
+ And can be used to express approval for [word2.definition]?
+ That's right, [word1] (mmm,[word2])!"
+
+ Word1 will always begin with 'm'. 
+ Word2 is word1 with the 'm' stripped.
+
+ This program should check a dictionary for a word beginning with 'm'.
+ When it finds one, it should strip the 'm'
+ and see if the remainder is a word in the dictionary.
+ If so, it uses it for one of these terrible jokes."""
